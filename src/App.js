9654a52b08a7bf974d51565502cc1e51f5c98505
Devansh Tiwari <devanshtiwari365@gmail.com>
2022-08-10 21:26:04 +0530

    :zap: Add routing
    Added routing functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { Route, Router, Routes } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import VotingPage from './pages/VotingPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+      <Routes>
+        <Route path="/" exact element={<HomePage />} />
+        <Route path="/login" exact element={<LoginPage />} />
+        <Route path="/register" exact element={<RegisterPage />} />
+        <Route path="/voting" exact element={<VotingPage />} />
+      </Routes>
   );
 }
 
