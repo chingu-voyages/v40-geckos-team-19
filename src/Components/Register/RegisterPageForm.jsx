3f00720b3f041e52a923520e4d458d0cd4741edf
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2022-08-22 21:42:34 +0400

    added http request to firebase

--- src/Components/Register/RegisterPageForm.jsx
+++ src/Components/Register/RegisterPageForm.jsx
@@ -8,16 +8,26 @@ const RegisterPageForm = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState();
 
-  //email check - no empty submission
+  //email check
   let regex = /^\S+@\S+\.\S+$/;
   const emailCheck = regex.test(email) === true;
   const emailIsValid = email.trim() !== "" && emailCheck;
 
-  //password check - no empty submission
+  //password check
   const passwordIsValid = password.trim() !== "";
+
+  //   const samePasswords = password === confirmPassword;
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmChange = (event) => {
+    setConfirmPassword(event.target.value);
   };
 
   const handleFormSubmit = (event) => {
@@ -27,11 +37,24 @@ const RegisterPageForm = () => {
       return;
     }
 
-    if (password !== confirmPassword) {
-      return;
-    }
+    console.log(email, password, confirmPassword);
 
-    console.log(email, password);
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAP16n5-hHHW2ZB_qYPvqaHQlX_kAScjIk",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+      }
+    );
+    //Add error handling
+
     setEmail("");
     setPassword("");
   };
@@ -54,6 +77,7 @@ const RegisterPageForm = () => {
           type="password"
           id="password-input"
           className="password-input"
+          onChange={handlePasswordChange}
           value={password}
           required
         />
@@ -64,12 +88,14 @@ const RegisterPageForm = () => {
           type="password"
           id="confirm-input"
           className="password-input"
+          onChange={handleConfirmChange}
           value={confirmPassword}
           required
         />
       </label>
       <div className="form-actions">
         <button type="submit">Register</button>
+        {/* {!samePasswords && <p>Your passwords don't match</p>} */}
       </div>
     </form>
   );
