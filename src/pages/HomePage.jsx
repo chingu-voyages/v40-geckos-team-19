ef27d719a41f81bb2775c1414cf82558044e9ef5
Devansh Tiwari <devanshtiwari365@gmail.com>
2022-09-01 02:28:00 +0530

    :sparkles: Add homepage
    add homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import CTA from '../components/CTA'
+import Footer from '../components/Footer'
+import ImageSlider from '../components/ImageSlider/ImageSlider'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <ImageSlider />
+      <CTA />
+      <Footer />
+    </div>
   )
 }
+
+
 
 export default HomePage
