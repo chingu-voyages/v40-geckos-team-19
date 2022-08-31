3a2dade10d8ac1bb93d8ce9d66efe4aa435e1296
Devansh Tiwari <devanshtiwari365@gmail.com>
2022-09-01 02:26:47 +0530

    :sparkles: Add cta & footer components
    added cta & footer

---
+++ src/components/CTA/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './CTA.css'
+
+const CTA = () => {
+  return (
+    <div className='cta_container'>
+        <div>
+            <h3>Always be <br /> inspire</h3>
+            <p>Receive updates about new <br /> and best design posts.</p>
+            <button className='btn'>Join Art Archive</button>
+        </div>
+    </div>
+  )
+}
+
+export default CTA
