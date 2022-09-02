59eff75cbef0f23faafc6a17bb22d488b96fd3b9
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2022-09-02 14:03:05 +0400

    edited text

--- src/components/CTA/index.jsx
+++ src/components/CTA/index.jsx
@@ -5,9 +5,9 @@ const CTA = () => {
   return (
     <div className='cta_container'>
         <div>
-            <h3>Always be <br /> inspire</h3>
+            <h3>Always be <br /> inspired</h3>
             <p>Receive updates about new <br /> and best design posts.</p>
-            <button className='btn'>Join Art Archive</button>
+            <button className='btn'>Join WeDesign</button>
         </div>
     </div>
   )
