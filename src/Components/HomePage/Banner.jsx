dc90d68618d06fc1a08d06ab4e0fa395ba0aca95
Ashutosh Singh <ashutosh2564@gmail.com>
2022-09-10 07:45:04 +0530

    Added Homepage
    Homepage along with header and banner

---
+++ src/Components/HomePage/Banner.jsx
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import Channel1 from "../../assets/item1.svg";
+import Channel2 from "../../assets/item2.svg";
+import './Banner.css' ;
+
+const banner = {
+  animate: {
+    transition: {
+      delayChildren: 0.4,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const letterAni = {
+  initial: { y: 400 },
+  animate: {
+    y: 0,
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.95],
+      duration: 1,
+    },
+  },
+};
+
+const Banner = () => {
+  const [ setPlayMarquee] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setPlayMarquee(true);
+    }, 2000);
+  }, );
+  return (
+    <motion.div className='banner' variants={banner}>
+      <BannerRowTop title={""} />
+    
+    </motion.div>
+  );
+};
+
+const AnimatedLetters = ({ title, disabled }) => (
+  <motion.span
+    className='row-title'
+    variants={disabled ? null : banner}
+    initial='initial'
+    animate='animate'>
+    {[...title].map((letter) => (
+      <motion.span
+        className='row-letter'
+        variants={disabled ? null : letterAni}>
+        {letter}
+      </motion.span>
+    ))}
+  </motion.span>
+);
+
+const BannerRowTop = ({ title }) => {
+  return (
+    <div className={"banner-row"}>
+      <div className='row-col'>
+       
+        <AnimatedLetters title={title} />
+      </div>
+      <motion.div
+        initial={{ opacity: 0, y: 80 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          ease: "easeInOut",
+          duration: 1,
+          delay: 0.4,
+        }}
+        className='row-col'>
+        <span className='rowmessage'>
+        <h1>Show your art</h1>
+          <h3> Show your artworks and <br/>
+          get appreciated </h3>
+        <img className="banner" src={Channel1} alt="logo" />
+        <img className="banner" src={Channel2} alt="logo" />
+        </span>
+        <div className="pic">
+        
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+
+export default Banner;
