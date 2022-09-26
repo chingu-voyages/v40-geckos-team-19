7a7a52a5b1eb589311faaeead6ee3e484ca73e98
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2022-09-26 13:37:37 +0400

    removed router import as not being used

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Fragment, useContext } from "react";
-import { Route, Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
