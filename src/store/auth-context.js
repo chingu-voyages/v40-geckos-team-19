4c4e898b82b13d85bcc1e4446134538241ee1f6c
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2022-09-30 15:29:11 +0400

    removed console log

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -76,7 +76,6 @@ export const AuthContextProvider = (props) => {
 
   useEffect(() => {
     if (tokenData) {
-      console.log(tokenData.duration);
       logoutTimer = setTimeout(logoutHandler, tokenData.duration);
     }
   }, [tokenData, logoutHandler]);
